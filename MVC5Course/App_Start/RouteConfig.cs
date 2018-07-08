e996140fd7d1cdba431e245ca079a2711bd25b32
Will <doggy.huang@gmail.com>
2018-07-08 14:52:06 +0800

    將 ASP.NET MVC 網站一秒變 PHP

--- MVC5Course/App_Start/RouteConfig.cs
+++ MVC5Course/App_Start/RouteConfig.cs
@@ -15,7 +15,7 @@ namespace MVC5Course
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "{controller}/{action}.php/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
