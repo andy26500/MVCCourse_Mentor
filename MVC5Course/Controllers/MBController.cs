3ad7e3af47e79325c46f609a2614b0e975147296
Will <doggy.huang@gmail.com>
2018-07-14 10:22:25 +0800

    加入 MBController

---
+++ MVC5Course/Controllers/MBController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class MBController : Controller
+    {
+        // GET: MB
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
