85b1ae2e61bd9de4772d3e7b595eace9f9b43ed0
Will <doggy.huang@gmail.com>
2018-07-15 14:26:03 +0800

    示範幾個簡化 View 取值的方法

---
+++ MVC5Course/Helpers/Utils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Helpers
+{
+    public static class Utils
+    {
+        public static string GetRouteValue2(this WebViewPage view, string name)
+        {
+            return view.ViewContext.RouteData.Values[name]?.ToString();
+        }
+        public static string GetModelStateValue2(this WebViewPage view, string name)
+        {
+            return view.ViewContext.ViewData.ModelState[name]?.Value.AttemptedValue;
+        }
+    }
+}
