694d99527371f034a8810d65fed46bdde7424be4
Will <doggy.huang@gmail.com>
2018-07-29 14:28:17 +0800

    示範 Area 用法

--- MVC5Course/App_Start/RouteConfig.cs
+++ MVC5Course/App_Start/RouteConfig.cs
@@ -18,7 +18,11 @@ namespace MVC5Course
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[]
+                {
+                    "MVC5Course.Controllers"
+                }
             );
 
         }
