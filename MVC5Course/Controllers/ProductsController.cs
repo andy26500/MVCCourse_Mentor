296ad77adb6ddfa218ee1c3995ece60ff21e8353
Will <doggy.huang@gmail.com>
2018-07-29 13:27:09 +0800

    39 練習使用 X.PagedList.Mvc 進行資料分頁

--- MVC5Course/Controllers/ProductsController.cs
+++ MVC5Course/Controllers/ProductsController.cs
@@ -9,18 +9,18 @@ using System.Web;
 using System.Web.Mvc;
 using MVC5Course.Models;
 using Omu.ValueInjecter;
+using X.PagedList;
 
 namespace MVC5Course.Controllers
 {
     public class ProductsController : BaseController
     {
         // GET: Products
-        public ActionResult Index()
+        public ActionResult Index(int pageNo = 1)
         {
             var data = db.Product
-                .OrderByDescending(p => p.ProductId);
-
-
+                .OrderByDescending(p => p.ProductId)
+                .ToPagedList(pageNumber: pageNo, pageSize: 10);
 
             return View(data);
         }
