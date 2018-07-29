eda79c447c80c2ec6abce44fa4f1d72d6a81f62b
Will <doggy.huang@gmail.com>
2018-07-29 14:40:52 +0800

    在 Area 的路由也加上 namespaces 設定

--- MVC5Course/Areas/Admin/AdminAreaRegistration.cs
+++ MVC5Course/Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,11 @@ namespace MVC5Course.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[]
+                {
+                    "MVC5Course.Areas.Admin.Controllers"
+                }
             );
         }
     }
